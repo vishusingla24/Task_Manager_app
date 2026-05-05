37acb9e93b57330f8e72d771bace588f5206671b
Vishu Singla <vishusingla24@gmail.com>
2026-05-05 21:00:28 +0530

    fixed calendar eslint + bug

--- FrontEnd/src/components/Calendar/Calendar.js
+++ FrontEnd/src/components/Calendar/Calendar.js
@@ -7,7 +7,7 @@ import Badge from "@mui/material/Badge";
 import { PickersDay } from "@mui/x-date-pickers/PickersDay";
 import CheckIcon from "@mui/icons-material/Check";
 import "./Calendar.css";
-
+/*
 const Calendar = () => {
   const [value, setValue] = useState(new Date());
   const [highlightedDays, setHighlightedDays] = useState([1, 2, 13]);
@@ -43,4 +43,38 @@ const Calendar = () => {
   );
 };
 
+export default Calendar;
+*/
+const Calendar = () => {
+  const [value, setValue] = useState(new Date());
+  const [highlightedDays] = useState([1, 2, 13]);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDatePicker
+        variant="static"
+        orientation="portrait"
+        value={value}
+        disableFuture
+        onChange={(newValue) => setValue(newValue)}
+        renderInput={(params) => <TextField {...params} />}
+        renderDay={(day, _value, DayComponentProps) => {
+          const isSelected =
+            !DayComponentProps.outsideCurrentMonth &&
+            highlightedDays.indexOf(day.getDate()) >= 0;
+
+          return (
+            <Badge
+              key={day.toString()}
+              overlap="circular"
+              badgeContent={isSelected ? <CheckIcon color="red" /> : undefined}
+            >
+              <PickersDay {...DayComponentProps} />
+            </Badge>
+          );
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
 export default Calendar;
